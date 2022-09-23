5e5a47787478e9a337df18971d0173b001623874
houyuewei <houyaowei@163.com>
2022-09-23 09:47:26 +0800

    test

---
+++ packages/basic-utils/lib/basic-utils.js
@@ -0,0 +1,17 @@
+import _ from 'lodash'
+import { isArray } from 'javascript-validate-utils';
+
+function uniqArray(arr) {
+    if(isArray(arr)) {
+        return _.uniq(arr)
+    } else {
+        return []
+    }
+}
+function fn(){
+
+}
+export {
+    uniqArray,
+    fn
+}
